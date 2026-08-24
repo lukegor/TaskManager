2f662d091ff1d70fa31a8a12fa18a15a8c985c93
lukegor <105490868+lukegor@users.noreply.github.com>
2026-08-24 15:14:48 +0200

    refactor: Process and ProcessItem use ObservableObject

--- src/TaskManager.Domain/Models/Process.cs
+++ src/TaskManager.Domain/Models/Process.cs
@@ -1,6 +1,5 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
 using TaskManager.Domain.Primitives;
 
 namespace TaskManager.Domain.Models
@@ -9,71 +8,29 @@ namespace TaskManager.Domain.Models
     /// Represents Windows System Process data (core business logic).
     /// Raises per-field change notifications so bound grid rows update in place.
     /// </summary>
-    public class Process : IExportable, INotifyPropertyChanged
+    public partial class Process : ObservableObject, IExportable
     {
+        [ObservableProperty]
         private string _name = string.Empty;
-        private string _path = string.Empty;
+
+        [ObservableProperty]
         private int? _pid;
-        private ArchitectureType _architectureType;
+
+        [ObservableProperty]
+        private string _path = string.Empty;
+
+        [ObservableProperty]
         private int? _priority;
+
+        [ObservableProperty]
         private int _threadCount;
+
+        [ObservableProperty]
         private int? _ppid;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public required string Name
-        {
-            get => _name;
-            set => SetField(ref _name, value);
-        }
-
-        public int? Pid
-        {
-            get => _pid;
-            set => SetField(ref _pid, value);
-        }
-
-        [IgnoreSerialization]
-        [JsonIgnore]
-        public ArchitectureType ArchitectureType
-        {
-            get => _architectureType;
-            set
-            {
-                if (_architectureType == value)
-                {
-                    return;
-                }
-
-                _architectureType = value;
-                Notify(nameof(ArchitectureType));
-                Notify(nameof(ArchitectureTypeDisplay));
-            }
-        }
-
-        public required string Path
-        {
-            get => _path;
-            set => SetField(ref _path, value);
-        }
-
-        public int? Priority
-        {
-            get => _priority;
-            set => SetField(ref _priority, value);
-        }
-
-        public int ThreadCount
-        {
-            get => _threadCount;
-            set => SetField(ref _threadCount, value);
-        }
-
-        public int? Ppid
-        {
-            get => _ppid;
-            set => SetField(ref _ppid, value);
-        }
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(ArchitectureTypeDisplay))]
+        private ArchitectureType _architectureType;
 
         [IgnoreSerialization]
         [JsonIgnore]
@@ -91,19 +48,5 @@ namespace TaskManager.Domain.Models
                 ThreadCount,
                 Ppid);
         }
-
-        private void SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value))
-            {
-                return;
-            }
-
-            field = value;
-            Notify(propertyName!);
-        }
-
-        private void Notify(string propertyName)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- src/TaskManager.Domain/Models/ProcessItem.cs
+++ src/TaskManager.Domain/Models/ProcessItem.cs
@@ -1,6 +1,5 @@
-using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 
 namespace TaskManager.Domain.Models
 {
@@ -9,26 +8,12 @@ namespace TaskManager.Domain.Models
     /// <see cref="IsSelected"/> is the single source of truth for row selection:
     /// both the DataGrid row style and the select-checkbox bind to it TwoWay.
     /// </summary>
-    public class ProcessItem : INotifyPropertyChanged
+    public partial class ProcessItem : ObservableObject
     {
         public required Process Process { get; set; }
 
-        public bool IsSelected
-        {
-            get;
-            set
-            {
-                if (field == value)
-                {
-                    return;
-                }
-
-                field = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
+        [ObservableProperty]
+        private bool _isSelected;
 
         [SetsRequiredMembers]
         public ProcessItem(Process process)
@@ -40,8 +25,5 @@ namespace TaskManager.Domain.Models
         {
             return $"{Process.Name} ({Process.Pid})";
         }
-
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
